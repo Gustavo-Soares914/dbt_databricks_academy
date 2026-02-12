ebaabd597d9fccc107858476d5cacc09ad1df1ae
Gustavo Soares <soares.gustavo914@gmail.com>
2026-02-12 17:17:09 +0000

    Alteração de nomes na stg_person__businessentityaddress

--- models/staging/staging/DBT_adventure_works/stg_person__businessentityaddress.sql
+++ models/staging/staging/DBT_adventure_works/stg_person__businessentityaddress.sql
@@ -7,9 +7,9 @@ source_address as (
 
 renamed as (
     select  
-        cast(businessentityid as int) as business_entity_id
-        , cast (addressid as int) as address_id
-        , cast (addresstypeid as int) as address_type_id
+        cast(businessentityid as int) as business_entity_id_pk
+        , cast (addressid as int) as address_id_pk
+        , cast (addresstypeid as int) as address_type_id_pk
 
     from source_address
 )
